f1fcd6f5a6fda3c651d5fe258c591e1e8b404cea
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2021-02-13 20:30:23 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Alfredo Dela Cruz Jr. on 2/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
