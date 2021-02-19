5407267294add406f845067f0e50d15901347b6d
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2021-02-18 19:11:04 -0800

    added a cell to give more info about a movie. and another screen for movie posters

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Alfredo Dela Cruz Jr. on 2/18/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    var movie: [String: Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        titleLabel.text = movie["title"] as? String
+        
+        synopsisLabel.text = movie["overview"] as? String
+        
+        //no auto layout so fits characters in
+        titleLabel.sizeToFit()
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        
+        //posterView
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        //backdrop
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath) //change background resolution to 780
+        backdropView.af.setImage(withURL: backdropUrl!)
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Alfredo Dela Cruz Jr. on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
